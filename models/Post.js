01bbfa30ce3c50484d3f63bd53f9fafa66a1e422
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-12-22 20:59:08 -0600

    created proper type def and query, set up resolver to get posts, server is showing dummy data from mongodb, server setup is complete!

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,7 @@
 const { model, Schema } = require('mongoose');
 
 const postSchema = new Schema({
-    Body: String,
+    body: String,
     username: String,
     createdAt: String,
     comments: [
