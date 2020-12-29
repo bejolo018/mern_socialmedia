3edca39995df199931bd34f996b9bf7d9fac9882
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-12-28 23:36:25 -0600

    implemented react router and semantic ui, created skeleton pages and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
+import MenuBar from './Components/MenuBar'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 function App() {
   return (
-    <div>
-      dasda
-    </div>
+    <Router>
+      <Container>
+      <MenuBar />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
   );
 }
 
