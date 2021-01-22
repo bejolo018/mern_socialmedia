2de186192f7a536a98c48cfb3b9ed90cdc4ef51d
bejolo018 <bejolo018@gmail.com>
2021-01-21 18:15:40 -0600

    created context for when user is logged in, applied it in login and register as well wrapped the app in the context provider, app recieving data in dev tools!

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
+import { AuthProvider } from './Context/auth'
+
 import MenuBar from './Components/MenuBar'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
@@ -11,14 +13,16 @@ import Register from './Pages/Register'
 
 function App() {
   return (
-    <Router>
-      <Container>
-      <MenuBar />
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
