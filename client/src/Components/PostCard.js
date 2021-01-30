192d30497daa3e417841d9788fb768dfb1b021f8
bejolo018 <bejolo018@gmail.com>
2021-01-29 18:16:20 -0600

    added likebutton functionality

--- client/src/Components/PostCard.js
+++ client/src/Components/PostCard.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import { Card, Image, Button, Icon, Label } from 'semantic-ui-react'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
 
+import { AuthContext } from '../Context/auth'
+import LikeButton from './LikeButton'
+
 const PostCard = ({post: {body, createdAt, id, username, likeCount, commentCount, likes}}) => {
 
-    const likePost = () => {
-        console.log('Liked post!')
-    }
-
-    const commentPost = () => {
-        console.log('comment post!')
-    }
+    const { user } = useContext(AuthContext)
 
     return (
         <Card style={{backgroundColor: '#212121'}} fluid>
@@ -26,23 +23,21 @@ const PostCard = ({post: {body, createdAt, id, username, likeCount, commentCount
                 <Card.Description style={{color: 'white'}}>{body}</Card.Description>
             </Card.Content>
         <Card.Content extra>
-            <Button as='div' labelPosition='right' onClick={likePost}>
-                <Button color='purple' basic>
-                    <Icon name='heart' />
-                </Button>
-                    <Label as='a' color='purple' pointing='left'>
-                            {likeCount}
-                    </Label>
-            </Button>
+            <LikeButton user={user} post={{ id, likes, likeCount }} />
 
-            <Button as='div' labelPosition='right' onClick={commentPost}>
+            <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
                 <Button color='teal' basic>
                     <Icon name='comments' />
                 </Button>
-                    <Label as='a' color='teal' pointing='left'>
+                    <Label color='teal' pointing='left'>
                             {commentCount}
                     </Label>
             </Button>
+            {user && user.username === username && (
+                <Button as="div" color="red" floated="right" onClick={() => console.log('Delete Post')}>
+                    <Icon name="trash" style={{margin: 0}} />
+                </Button>
+            )}
         </Card.Content>
     </Card>
     )
