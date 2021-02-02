d063c199abced4b1d5394426b5e9d14f08d9fea9
bejolo018 <bejolo018@gmail.com>
2021-02-01 19:49:21 -0600

    Created delete button that displays modal to ask if user is sure

---
+++ client/src/Components/DeleteButton.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/client'
+import { Button, Confirm, Icon } from 'semantic-ui-react'
+
+const DeleteButton = ({ postId }) => {
+    const [confirmOpen, setConfirmOpen] = useState(false)
+
+    const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+        update(){
+            setConfirmOpen(false)
+        },
+        variables: {
+            postId
+        }
+    })
+
+    return (
+        <div>
+        <Button as="div" color="red" floated="right" onClick={() => setConfirmOpen(true)}>
+                    <Icon name="trash" style={{margin: 0}} />
+        </Button>
+        <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePost} ></Confirm>
+        </div>
+    )
+}
+
+const DELETE_POST_MUTATION = gql`
+    mutation deletePost($postId: ID!){
+        deletePost(postId: $postId)
+    }
+`
+
+export default DeleteButton
